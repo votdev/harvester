48be40ed7f3af6c04f71e1f708449d488eb03de4
Volker Theile <vtheile@suse.com>
2024-01-25 15:20:51 +0100

    Add DownloadURL controller for testing purpose
    Signed-off-by: Volker Theile <vtheile@suse.com>

---
+++ pkg/apis/harvesterhci.io/v1beta1/downloadurl.go
@@ -0,0 +1,17 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:printcolumn:name="URL",type="string",JSONPath=`.url`
+
+type DownloadURL struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// +kubebuilder:validation:Required
+	URL string `json:"url,omitempty"`
+}
