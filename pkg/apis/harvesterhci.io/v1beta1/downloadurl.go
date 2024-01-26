385b49bd8aa7256a2376059f0a5cd0ecd6469539
Volker Theile <vtheile@suse.com>
2024-01-26 12:31:06 +0100

    Add status support
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- pkg/apis/harvesterhci.io/v1beta1/downloadurl.go
+++ pkg/apis/harvesterhci.io/v1beta1/downloadurl.go
@@ -4,15 +4,29 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const (
+	INIT_STATE        = "INIT"
+	DOWNLOADING_STATE = "DOWNLOADING"
+	DONE_STATE        = "DONE"
+)
+
+type DownloadURLStatus struct {
+	// +kubebuilder:default:=INIT
+	Status string `json:"status,omitempty" default:"INIT"`
+}
+
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:resource:shortName=durl;durls,scope=Namespaced
 // +kubebuilder:printcolumn:name="URL",type="string",JSONPath=`.url`
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.status`
+// +kubebuilder:subresource:status
 
 type DownloadURL struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// +kubebuilder:validation:Required
-	URL string `json:"url,omitempty"`
+	URL    string            `json:"url,omitempty"`
+	Status DownloadURLStatus `json:"status,omitempty"`
 }
--- pkg/controller/master/downloadurl/downloadurl_controller.go
+++ pkg/controller/master/downloadurl/downloadurl_controller.go
@@ -3,12 +3,14 @@ package downloadurl
 import (
 	"context"
 	"net/http"
+	"reflect"
 	"time"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	"github.com/harvester/harvester/pkg/config"
+	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
 )
 
 const (
@@ -18,6 +20,7 @@ const (
 func Register(ctx context.Context, management *config.Management, options config.Options) error {
 	downloadURLController := management.HarvesterFactory.Harvesterhci().V1beta1().DownloadURL()
 	handler := &handler{
+		ctl: downloadURLController,
 		httpClient: http.Client{
 			Timeout: 15 * time.Second,
 		},
@@ -28,22 +31,36 @@ func Register(ctx context.Context, management *config.Management, options config
 
 // handler tries to download from specified URL on DownloadURL changes.
 type handler struct {
+	ctl        ctlharvesterv1.DownloadURLController
 	httpClient http.Client
 }
 
-func (h *handler) OnChange(_ string, downloadURL *v1beta1.DownloadURL) (*v1beta1.DownloadURL, error) {
+func (h *handler) OnChange(_ string, downloadURL *harvesterv1.DownloadURL) (*harvesterv1.DownloadURL, error) {
 	if downloadURL == nil || downloadURL.DeletionTimestamp != nil {
 		return nil, nil
 	}
 
-	logrus.Debugf("download from URL: %s", downloadURL.URL)
-	resp, err := h.httpClient.Get(downloadURL.URL)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+	downloadURLCopy := downloadURL.DeepCopy()
 
-	// then operates the response body
+	switch downloadURL.Status.Status {
+	case "":
+		downloadURLCopy.Status.Status = harvesterv1.INIT_STATE
+	case harvesterv1.INIT_STATE:
+		downloadURLCopy.Status.Status = harvesterv1.DOWNLOADING_STATE
+		logrus.Debugf("download from URL: %s", downloadURL.URL)
+		resp, err := h.httpClient.Get(downloadURL.URL)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		// then operates the response body
+	case harvesterv1.DOWNLOADING_STATE:
+		downloadURLCopy.Status.Status = harvesterv1.DONE_STATE
+	}
+
+	if !reflect.DeepEqual(downloadURLCopy, downloadURL) {
+		return h.ctl.UpdateStatus(downloadURLCopy)
+	}
 
 	return downloadURL, nil
 }
